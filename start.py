3d0367e07c1cf9c4a263d1111ceca9d8a840c94b
hank0438 <hank0438@gmail.com>
2020-07-04 16:29:52 +0800

    well-done start.py

--- start.py
+++ start.py
@@ -2,39 +2,29 @@ import argparse
 from analysis.tracer.tracer import trace
 from analysis.gen_pocs import gen_pocs
 
-# case = "fastbin_dup"
-# case = "house_of_spirit"
-# case = "unsafe_unlink"
-case = "tcache_poisoning"
-folder = "./tests/how2heap_" + case + "/"
-
-config = folder + "analysis.yaml"
-binary = folder + case + ".bin"
-source = folder + case + ".c"
-desc = folder + case + ".bin-desc.yaml"
-result = folder + case + ".bin-result.yaml"
-
-# config = "./tests/how2heap_test/analysis.yaml"
-# binary = "./tests/how2heap_test/heap_test.bin"
-
-
-
-# trace(config, binary)
-# gen_pocs(config, binary, result, desc, source)
+'''
+usage:
+ptmalloc:
+    python3 start.py trace -a how2heap -e tcache_poisoning
+jemalloc
+    python3 start.py trace -a jemalloc -e arbitary_free
+'''
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Find heap corruptions')
-    parser.add_argument('action', metavar='<identify|gen|trace|poc|other>', type=str,
-                        choices=['identify', 'trace', 'gen', 'poc','other'],
+    parser.add_argument('action', metavar='<gen|trace|poc>', type=str,
+                        choices=['trace', 'gen', 'poc'],
                         help='Please give me an action')
     parser.add_argument('-e', '--exploit', metavar='exploit_model', type=str,
-                        help='Please give me an exploit model')
+                        help='Please give me an exploit model', default='tcache_poisoning')
 
+    parser.add_argument('-a', '--allocator', metavar='allocator', type=str,
+                        help='Please give me an allocator', default='how2heap')
     args = parser.parse_args()
-    # case = "tcache_poisoning"
+    allocator = args.allocator
     case = args.exploit
-    folder = "./tests/how2heap_" + case + "/"
 
+    folder = "./tests/" + allocator + "_" + case + "/"
     config = folder + "analysis.yaml"
     binary = folder + case + ".bin"
     source = folder + case + ".c"
@@ -49,12 +39,7 @@ if __name__ == '__main__':
         if config is None or binary is None or result is None or desc is None or source is None:
             parser.error('config or binary or result or desc or source path is wrong')
         gen_pocs(config, binary, result, desc, source)
-    elif args.action == 'other':
-        # case = "double_free"
-        case = "arbitary_free"
-        folder = "./tests/jemalloc_" + case + "/"
-        config = folder + "analysis.yaml"
-        binary = folder + case + ".bin"
-        trace(config, binary)
+    # elif args.action == 'gen':
+    #     print("gen code")
     else:
         parser.error('require an action')
